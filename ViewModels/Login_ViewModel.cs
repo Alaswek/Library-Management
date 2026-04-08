9dd2239c07aa233a9856a18642d71b52ad0344ed
Amihaiesei Andrei-Casian <casianm8@yahoo.com>
2026-04-08 17:57:18 +0300

    Update ~ Base project

---
+++ ViewModels/Login_ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GenericUi.Commands;
+using LibraryManagement.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LibraryManagement.ViewModels
+{
+    public class Login_ViewModel : ViewModelBase
+    {
+
+
+        #region iCommands
+        public ICommand Login_command { get; }
+        #endregion
+
+        public Login_ViewModel()
+        {
+            // constructor 
+            Login_command = new RelayCommand(_ => DoLogin());
+        }
+
+
+        public void DoLogin()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
