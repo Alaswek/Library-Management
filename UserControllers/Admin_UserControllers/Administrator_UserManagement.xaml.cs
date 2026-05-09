1f830e0bc10681e393ea1a54f0106855453a332b
andreicaba377 <cabaandrei610@gmail.com>
2026-05-09 20:57:04 +0300

    update
    am facut interfata pentru user management

---
+++ UserControllers/Admin_UserControllers/Administrator_UserManagement.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace LibraryManagement.UserControllers.Admin_UserControllers
+{
+    public partial class Administrator_UserManagement : UserControl
+    {
+        public Administrator_UserManagement()
+        {
+            // Această metodă este vitală! Ea caută fișierul XAML și desenează interfața.
+            InitializeComponent();
+        }
+    }
+}
